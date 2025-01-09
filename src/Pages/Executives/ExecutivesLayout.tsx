a66292610680a878ab2850abd0708e815287d9a5
Diabene <addodiabene69@gmail.com>
2025-01-09 16:42:23 +0000

    Update ExecutivesLayout.tsx

--- src/Pages/Executives/ExecutivesLayout.tsx
+++ src/Pages/Executives/ExecutivesLayout.tsx
@@ -13,7 +13,7 @@ type sideBarContextProps = {
   YEARS: string[];
 };
 export const SidebarHandlerContext = createContext({} as sideBarContextProps);
-const YEARS = ["2024", "2023", "2022"];
+const YEARS = ["2025", "2024", "2023", "2022"];
 
 function ExecutivesLayout() {
   const [openSidebar, setOpenSidebar] = useState(false);
